179ecfb29112017e12ea86bc7e4695fd9d00ff7f
pavanarepu <larepu@tistatech.com>
2019-06-19 21:53:47 -0500

    Scala code formatting plugin

--- tweet-analysis/app/controllers/TweetInfoAPIController.scala
+++ tweet-analysis/app/controllers/TweetInfoAPIController.scala
@@ -1,15 +1,21 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
+import services.TweetStreamService
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class TweetInfoAPIController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class TweetInfoAPIController @Inject()(
+    cc: ControllerComponents,
+    tweetService: TweetStreamService
+) extends AbstractController(
+      cc
+    ) {
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +24,18 @@ class TweetInfoAPIController @Inject()(cc: ControllerComponents) extends Abstrac
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+  def index() =
+    Action {
+      implicit request: Request[
+        AnyContent
+      ] =>
+        tweetService
+          .getInfo() map { msg =>
+          Ok(
+            Json.toJson(
+              msg
+            )
+          )
+        } getOrElse NotFound
+    }
 }
